a094d838ed090a2349c60c4a90e2a29cc094c18f
wooosh <12696312+wooosh@users.noreply.github.com>
2020-07-18 21:38:57 +0000

    parser stub

--- src/main.c
+++ src/main.c
@@ -1,14 +1,39 @@
 #include "lexer.h"
 
+// @TODO: type system
+// @TODO: parser
+// @TODO: backend
+
 typedef struct token_buf {
     token* tokens;
-    size_t index;
+    size_t idx;
 } token_buf;
 
+// @TODO: check for EOF
+token tb_pop(token_buf* tb) {
+    tb->idx++;
+    return tb->tokens[tb->idx-1];
+}
+
+typedef struct function {
+    // @TODO: parameters
+    // @TODO: return value
+    char* name;
+} function;
+
+void parse(token_buf* tb) {
+    // Continually try to parse functions
+    token t;
+    while(1) {
+        t = tb_pop(tb);
+        if (t.type != t_identifier) printf("%s\n", token_location(t));
+	if (t.type == t_EOF) break;
+    }
+}
 
 int main(int argc, char** argv) {
-    token_buf toks = {lex(argv[1]), 0};
+    token_buf tb = {lex(argv[1]), 0};
+    parse(&tb);
 
-//    printf("test\n");
     return 0;
 }
